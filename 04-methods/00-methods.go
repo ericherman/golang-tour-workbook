1b8b4aa69008456784455d88577251a833c4640c
Eric Herman <eric@freesa.org>
2016-03-18 21:20:47 +0100

    methods are functions on structs
    http://tour.golang.org/methods/1

---
+++ 04-methods/00-methods.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type point2d struct {
+	x, y float64
+}
+
+func (v point2d) Abs() float64 {
+	return math.Sqrt(v.x*v.x + v.y*v.y)
+}
+
+func main() {
+	v := point2d{3, 4}
+	fmt.Println(v.Abs())
+}
